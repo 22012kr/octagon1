7f9ebdebc2b82f630d82ffacff27553745a291b4
Колесников Александр Сергеевич <22012@iat.iat>
2026-02-26 22:46:44 +0800

    Исправление

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-/*const http = require("http");
-
-http.createServer(function(request, response){
-    response.end("Привет, Октагон!");
-}).listen(3000, "localhost",function(){
-    console.log("Сервер запущен");
-});*/
-
 const express = require("express");
 
 const app = new express();
