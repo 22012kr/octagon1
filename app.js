29e475a64d0115f40997de1ece07cbfb725c9f69
Колесников Александр Сергеевич <22012@iat.iat>
2026-03-03 14:44:13 +0800

    Подгрузка 2-й компетенции Октагон

--- app.js
+++ app.js
@@ -7,4 +7,32 @@ app.get("/", function(request, response)
     response.send("<h1>Привет, Октагон!<h1>");
 });
 
+app.use(express.json());
+
+app.use("/static", function(request, response){
+    response.json({header: "Hello",
+        body: "Octagon NodeJs Test"
+    });
+});
+
+app.use("/dynamic", function(request, response){
+    const a = request.query.a;
+    const b = request.query.b;
+    const c = request.query.c;
+
+    let arr = [a, b, c];
+
+    for (i = 0; i < arr.length; ++i)
+    {
+        if (arr[i] == undefined || !Number(arr[i]))
+        {
+            response.json({header: "Error"}); break;
+        } else continue;
+    }
+
+    const s = +a + +b + +c;
+    response.json({header: "Calculated", 
+        body: s});
+});
+
 app.listen(3000);
